91abc8bfbe66679ddb1a9cdea89701158bf81d91
yqwoe <yqwoe@live.cn>
2018-10-03 19:49:26 +0800

    初始化微信公众号

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,15 @@
 class HomeController < ApplicationController
 
   def index
+    p params
     if check_signature?(params[:signature],params[:timestamp],params[:nonce])
       return render json: params[:echostr]
     end
     # render json: {version: '1.0.0',name: '嘻唰唰'}
+  end
+
+  def create
+    p params
   end
 
   private
