afeeb49026fc90182f790472816fa3d7b6acf6e5
yqwoe <yqwoe@live.cn>
2018-10-04 12:57:09 +0800

    部署

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181003130937) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "link"
